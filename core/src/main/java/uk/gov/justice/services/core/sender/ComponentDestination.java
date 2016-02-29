b8a3da40a1adf6fe407ab163beaceb8fb5989836
Zeeshan Ghalib <zeeshan.ghalib@ext.soprasteria.com>
2016-02-29 10:49:23 +0000

    Add Sender service
    Used by the handlers to send envelope to the next service component.

---
+++ core/src/main/java/uk/gov/justice/services/core/sender/ComponentDestination.java
@@ -0,0 +1,41 @@
+package uk.gov.justice.services.core.sender;
+
+import uk.gov.justice.services.core.annotation.Component;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static uk.gov.justice.services.core.annotation.Component.COMMAND_API;
+import static uk.gov.justice.services.core.annotation.Component.COMMAND_CONTROLLER;
+import static uk.gov.justice.services.core.annotation.Component.COMMAND_HANDLER;
+
+/**
+ * Contains the relationship between the various service components in the same pillar.
+ */
+@ApplicationScoped
+public class ComponentDestination {
+
+    private final Map<Component, Component> destinationComponentMap;
+
+    public ComponentDestination() {
+        destinationComponentMap = new ConcurrentHashMap<>();
+        destinationComponentMap.put(COMMAND_API, COMMAND_CONTROLLER);
+        destinationComponentMap.put(COMMAND_CONTROLLER, COMMAND_HANDLER);
+    }
+
+    /**
+     * Get the default destination component for <code>component</code>.
+     *
+     * @param component the component whose destination {@link Component} is required.
+     * @return the default destination {@link Component} for <code>component</code>.
+     */
+    public Component getDefault(final Component component) {
+        if (!destinationComponentMap.containsKey(component)) {
+            throw new IllegalArgumentException("No default destination defined for service component of type " + component);
+        }
+
+        return destinationComponentMap.get(component);
+    }
+
+}

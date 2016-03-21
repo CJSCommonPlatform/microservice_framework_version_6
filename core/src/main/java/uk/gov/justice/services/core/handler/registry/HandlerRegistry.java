2f82d7226f6248117ad08db5a0cd4ef139810cf0
Shaun Francis <shaun.francis@agilesphere.co.uk>
2016-03-21 09:40:25 +0000

    Refactor HandlerRegistry and add new tests

--- core/src/main/java/uk/gov/justice/services/core/handler/registry/HandlerRegistry.java
+++ core/src/main/java/uk/gov/justice/services/core/handler/registry/HandlerRegistry.java
@@ -3,19 +3,27 @@ package uk.gov.justice.services.core.handler.registry;
 
 import uk.gov.justice.services.core.annotation.Handles;
 import uk.gov.justice.services.core.handler.HandlerInstanceAndMethod;
-import uk.gov.justice.services.core.handler.HandlerUtil;
 import uk.gov.justice.services.core.handler.registry.exception.DuplicateHandlerException;
 import uk.gov.justice.services.core.handler.registry.exception.InvalidHandlerException;
 import uk.gov.justice.services.messaging.Envelope;
 
 import java.lang.reflect.Method;
 import java.util.HashMap;
-import java.util.List;
+
+import static java.lang.String.format;
+import static java.util.Arrays.stream;
 
 /**
  * Service for storing a map of which command handlers handle which commands.
  */
 public class HandlerRegistry {
+
+    private static final String NO_METHOD_ANNOTATION_MESSAGE = "Class %s doesn't have any method annotated with the annotation @Handles";
+    private static final String MUST_HAVE_ONE_PARAMETER_MESSAGE = "Handles method must have exactly one parameter. Found %d";
+    private static final String MUST_RECEIVE_ENVELOPE_MESSAGE = "Handler methods must receive Envelope as an argument.";
+    private static final String DUPLICATE_METHOD_REGISTERED_MESSAGE = "Can not register %s, because a command handler method %s has already been registered for %s";
+
+    private static final Class<Handles> ANNOTATION_CLASS = Handles.class;
 
     private final HashMap<String, HandlerInstanceAndMethod> handlerMapping = new HashMap<>();
 
@@ -33,11 +41,24 @@ public class HandlerRegistry {
      * @param handlerInstance Handler instance to be registered.
      */
     public void register(final Object handlerInstance) {
-        final List<Method> handlerMethods = HandlerUtil.findHandlerMethods(handlerInstance.getClass(), Handles.class);
+        final long numberOfRegisteredMethods = registerAllAnnotatedMethodsFor(handlerInstance);
 
-        for (Method handlerMethod : handlerMethods) {
-            register(handlerInstance, handlerMethod);
+        if (numberOfRegisteredMethods == 0) {
+            throw new InvalidHandlerException(format(NO_METHOD_ANNOTATION_MESSAGE, handlerInstance.getClass().getName()));
         }
+    }
+
+    /**
+     * Registers all methods that are annotated as Handles and returns the number of registered methods
+     *
+     * @param handlerInstance Handler instance to register
+     * @return number of registered methods
+     */
+    private long registerAllAnnotatedMethodsFor(final Object handlerInstance) {
+        return stream(handlerInstance.getClass().getMethods())
+                .filter(method -> method.isAnnotationPresent(ANNOTATION_CLASS))
+                .map(method -> register(handlerInstance, method))
+                .count();
     }
 
     /**
@@ -45,26 +66,28 @@ public class HandlerRegistry {
      *
      * @param handler       Handler instance to register.
      * @param handlerMethod Handler method to register.
+     * @return true on completion
      */
-    private void register(final Object handler, final Method handlerMethod) {
+    private Boolean register(final Object handler, final Method handlerMethod) {
         if (handlerMethod.getParameterTypes().length != 1) {
-            throw new InvalidHandlerException("Handles method must have exactly one parameter. Found " + handlerMethod.getParameterTypes().length);
+            throw new InvalidHandlerException(format(MUST_HAVE_ONE_PARAMETER_MESSAGE, handlerMethod.getParameterTypes().length));
         }
 
         final Class<?> clazz = handlerMethod.getParameterTypes()[0];
         if (clazz != Envelope.class) {
-            throw new IllegalArgumentException("Handler methods must receive Envelope as an argument.");
+            throw new IllegalArgumentException(MUST_RECEIVE_ENVELOPE_MESSAGE);
         }
 
         HandlerInstanceAndMethod handlerInstanceAndMethod = new HandlerInstanceAndMethod(handler, handlerMethod);
 
-        String name = handlerMethod.getAnnotation(Handles.class).value();
+        String name = handlerMethod.getAnnotation(ANNOTATION_CLASS).value();
         if (handlerMapping.containsKey(name)) {
-            throw new DuplicateHandlerException("Can't register " + handlerInstanceAndMethod + ", because a command handler method "
-                    + handlerMapping.get(clazz) + " has already been registered for " + name);
+            throw new DuplicateHandlerException(format(DUPLICATE_METHOD_REGISTERED_MESSAGE, handlerInstanceAndMethod.toString(), handlerMapping.get(name).toString(), name));
         }
 
-        handlerMapping.put(handlerMethod.getAnnotation(Handles.class).value(), handlerInstanceAndMethod);
+        handlerMapping.put(handlerMethod.getAnnotation(ANNOTATION_CLASS).value(), handlerInstanceAndMethod);
+
+        return true;
     }
 
 }

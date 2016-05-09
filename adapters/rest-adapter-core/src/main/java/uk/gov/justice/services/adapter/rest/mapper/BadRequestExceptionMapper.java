8836f41e521c732d86b183835232a9fc72581410
Shaun Francis <shaun.francis@agilesphere.co.uk>
2016-05-09 10:30:30 +0100

    Remove Singleton annotation and add test

--- adapters/rest-adapter-core/src/main/java/uk/gov/justice/services/adapter/rest/mapper/BadRequestExceptionMapper.java
+++ adapters/rest-adapter-core/src/main/java/uk/gov/justice/services/adapter/rest/mapper/BadRequestExceptionMapper.java
@@ -4,12 +4,10 @@ import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
 
 import uk.gov.justice.services.adapter.rest.exception.BadRequestException;
 
-import javax.inject.Singleton;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
-@Singleton
 @Provider
 public class BadRequestExceptionMapper implements ExceptionMapper<BadRequestException> {
 
---
+++ adapters/rest-adapter-core/src/test/java/uk/gov/justice/services/adapter/rest/application/CommonProvidersTest.java
@@ -0,0 +1,20 @@
+package uk.gov.justice.services.adapter.rest.application;
+
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.Assert.assertThat;
+
+import uk.gov.justice.services.adapter.rest.mapper.BadRequestExceptionMapper;
+
+import java.util.Set;
+
+import org.junit.Test;
+
+public class CommonProvidersTest {
+
+    @Test
+    public void shouldReturnAllCommonProviders() throws Exception {
+        Set<Class<?>> providers = new CommonProviders().providers();
+        assertThat(providers, containsInAnyOrder(BadRequestExceptionMapper.class));
+    }
+
+}

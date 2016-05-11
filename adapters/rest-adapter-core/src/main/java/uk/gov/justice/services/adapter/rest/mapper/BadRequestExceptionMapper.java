779f1bea9353188bc7b24db47c5035a3ae5f852d
Zeeshan Ghalib <zeeshan.ghalib@gmail.com>
2016-05-11 15:17:45 +0100

    Add exception message to response body

--- adapters/rest-adapter-core/src/main/java/uk/gov/justice/services/adapter/rest/mapper/BadRequestExceptionMapper.java
+++ adapters/rest-adapter-core/src/main/java/uk/gov/justice/services/adapter/rest/mapper/BadRequestExceptionMapper.java
@@ -1,6 +1,9 @@
 package uk.gov.justice.services.adapter.rest.mapper;
 
+import static javax.json.Json.createObjectBuilder;
 import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+import static javax.ws.rs.core.Response.status;
+import static org.slf4j.LoggerFactory.getLogger;
 
 import uk.gov.justice.services.adapter.rest.exception.BadRequestException;
 
@@ -8,12 +11,22 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import org.slf4j.Logger;
+
 @Provider
 public class BadRequestExceptionMapper implements ExceptionMapper<BadRequestException> {
 
+    private static final Logger LOGGER = getLogger(BadRequestExceptionMapper.class);
+
     @Override
     public Response toResponse(final BadRequestException exception) {
-        return Response.status(BAD_REQUEST).build();
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("Bad Request", exception);
+        }
+
+        return status(BAD_REQUEST)
+                .entity(createObjectBuilder().add("error", exception.getMessage()).build().toString())
+                .build();
     }
 
 }
--- adapters/rest-adapter-core/src/test/java/uk/gov/justice/services/adapter/rest/mapper/BadRequestExceptionMapperTest.java
+++ adapters/rest-adapter-core/src/test/java/uk/gov/justice/services/adapter/rest/mapper/BadRequestExceptionMapperTest.java
@@ -1,6 +1,9 @@
 package uk.gov.justice.services.adapter.rest.mapper;
 
+import static com.jayway.jsonassert.JsonAssert.with;
 import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -12,11 +15,18 @@ import org.junit.Test;
 
 public class BadRequestExceptionMapperTest {
 
+    private static final String TEST_ERROR_MESSAGE = "Test Error Message.";
+
     @Test
     public void shouldReturn400ResponseForBadRequestException() throws Exception {
         BadRequestExceptionMapper exceptionMapper = new BadRequestExceptionMapper();
-        Response response = exceptionMapper.toResponse(new BadRequestException(""));
+
+        Response response = exceptionMapper.toResponse(new BadRequestException(TEST_ERROR_MESSAGE));
+
         assertThat(response.getStatus(), is(BAD_REQUEST.getStatusCode()));
+        assertThat(response.getEntity(), notNullValue());
+        with(response.getEntity().toString())
+                .assertThat("error", equalTo(TEST_ERROR_MESSAGE));
     }
 
 }

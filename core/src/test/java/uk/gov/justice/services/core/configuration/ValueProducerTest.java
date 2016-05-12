a9425fae81b5fb4ddc0120472138538d998a2cf5
Zeeshan Ghalib <zeeshan.ghalib@gmail.com>
2016-05-12 11:50:34 +0100

    Add Annotation support for properties

---
+++ core/src/test/java/uk/gov/justice/services/core/configuration/ValueProducerTest.java
@@ -0,0 +1,82 @@
+package uk.gov.justice.services.core.configuration;
+
+import static java.lang.String.format;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+import javax.enterprise.inject.spi.Annotated;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.naming.InitialContext;
+import javax.naming.NameNotFoundException;
+import javax.naming.NamingException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ValueProducerTest {
+
+    private static final String VALID_VALUE = "Valid value";
+    private static final String APP_NAME = "appName";
+    private static final String VALID_KEY = "valid key";
+    private static final String DEFAULT_VALUE = "Default Value";
+    private static final String EMPTY_VALUE = "";
+    @InjectMocks
+    private ValueProducer valueProducer;
+
+    @Mock
+    private InjectionPoint propertyInjectionPoint;
+
+    @Mock
+    private Annotated annotated;
+
+    @Mock
+    private Value param;
+
+    @Mock
+    InitialContext initialContext;
+
+    @Before
+    public void setup() throws NamingException {
+        valueProducer.appName = APP_NAME;
+        when(propertyInjectionPoint.getAnnotated()).thenReturn(annotated);
+        when(annotated.getAnnotation(Value.class)).thenReturn(param);
+        when(param.key()).thenReturn(VALID_KEY);
+    }
+
+    @Test
+    public void shouldReturnPropertyValue() throws NamingException {
+        when(initialContext.lookup(format("java:/app/%s/%s", APP_NAME, param.key()))).thenReturn(VALID_VALUE);
+
+        assertThat(valueProducer.produceValue(propertyInjectionPoint), equalTo(VALID_VALUE));
+    }
+
+    @Test
+    public void shouldReturnEmptyValue() throws NamingException {
+        when(initialContext.lookup(format("java:/app/%s/%s", APP_NAME, param.key()))).thenReturn(EMPTY_VALUE);
+
+        assertThat(valueProducer.produceValue(propertyInjectionPoint), equalTo(EMPTY_VALUE));
+    }
+
+    @Test
+    public void shouldReturnDefaultValueWhenNotFound() throws NamingException {
+        when(initialContext.lookup(format("java:/app/%s/%s", APP_NAME, param.key()))).thenThrow(NameNotFoundException.class);
+        when(param.defaultValue()).thenReturn(DEFAULT_VALUE);
+
+        assertThat(valueProducer.produceValue(propertyInjectionPoint), equalTo(DEFAULT_VALUE));
+    }
+
+    @Test(expected = MissingPropertyException.class)
+    public void shouldThrowExceptionWhenNotFoundAndNoDefaultValue() throws NamingException {
+        when(initialContext.lookup(format("java:/app/%s/%s", APP_NAME, param.key()))).thenThrow(NameNotFoundException.class);
+        when(param.defaultValue()).thenReturn(null);
+
+        valueProducer.produceValue(propertyInjectionPoint);
+    }
+
+}

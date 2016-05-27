e107c92acb44e2c744a0bb0d4e44e0e972cc7e75
Justin Cooke <jrrcooke@gmail.com>
2016-05-27 12:11:42 +0100

    Fix Optional MediaType in ResponderLoggerHelper

--- messaging/messaging-core/src/main/java/uk/gov/justice/services/messaging/logging/ResponseLoggerHelper.java
+++ messaging/messaging-core/src/main/java/uk/gov/justice/services/messaging/logging/ResponseLoggerHelper.java
@@ -2,6 +2,7 @@ package uk.gov.justice.services.messaging.logging;
 
 import javax.json.Json;
 import javax.json.JsonObjectBuilder;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Optional;
 
@@ -14,13 +15,13 @@ public final class ResponseLoggerHelper {
     public static String toResponseTrace(final Response response) {
 
         final Optional<Integer> responseCode = Optional.ofNullable(Integer.valueOf(response.getStatus()));
-        final Optional<String> mediaType = Optional.ofNullable(response.getMediaType().getType());
+        final Optional<MediaType> mediaType = Optional.ofNullable(response.getMediaType());
         final Optional<String> cppid = Optional.ofNullable(response.getHeaderString(ID));
 
         final JsonObjectBuilder builder = Json.createObjectBuilder();
 
         responseCode.ifPresent(integer -> builder.add("ResponseCode", integer));
-        mediaType.ifPresent(s -> builder.add("MediaType", s));
+        mediaType.ifPresent(s -> builder.add("MediaType", s.getType()));
         cppid.ifPresent(s -> builder.add(ID, s));
 
         return builder.build().toString();

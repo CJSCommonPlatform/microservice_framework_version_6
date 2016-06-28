justin-r-oliver/microservice_framework

Add tests and clean code

adapters/rest-adapter-generator/src/test/java/uk/gov/justice/services/adapters/rest/validator/BaseUriRamlValidatorTest.java
@@ -1,8 +1,9 @@
 package uk.gov.justice.services.adapters.rest.validator;
 
+import static uk.gov.justice.services.adapters.test.utils.builder.RamlBuilder.restRamlWithTitleVersion;
+
 import uk.gov.justice.raml.common.validator.RamlValidationException;
 import uk.gov.justice.raml.common.validator.RamlValidator;
-import uk.gov.justice.services.adapters.test.utils.builder.RamlBuilder;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -10,62 +11,45 @@ import org.junit.rules.ExpectedException;
 
 public class BaseUriRamlValidatorTest {
 
+    private RamlValidator validator = new BaseUriRamlValidator();
+
     @Rule
     public ExpectedException exception = ExpectedException.none();
-    private RamlValidator validator = new BaseUriRamlValidator();
 
     @Test
     public void shouldPassIfBaseUriContainsTheQueryPillarAndApiTier() throws Exception {
-        validator.validate(new RamlBuilder()
-                .withVersion("#%RAML 0.8")
-                .withTitle("Example Service")
-                .withBaseUri("http://localhost:8080/warname/query/api/rest/service")
-                .build());
+        validator.validate(restRamlWithTitleVersion()
+                .withBaseUri("http://localhost:8080/warname/query/api/rest/service").build());
     }
 
     @Test
     public void shouldPassIfBaseUriContainsTheQueryPillarAndControllerTier() throws Exception {
-        validator.validate(new RamlBuilder()
-                .withVersion("#%RAML 0.8")
-                .withTitle("Example Service")
-                .withBaseUri("http://localhost:8080/warname/query/controller/rest/service")
-                .build());
+        validator.validate(restRamlWithTitleVersion()
+                .withBaseUri("http://localhost:8080/warname/query/controller/rest/service").build());
     }
 
     @Test
     public void shouldPassIfBaseUriContainsTheQueryPillarAndViewTier() throws Exception {
-        validator.validate(new RamlBuilder()
-                .withVersion("#%RAML 0.8")
-                .withTitle("Example Service")
-                .withBaseUri("http://localhost:8080/warname/query/view/rest/service")
-                .build());
+        validator.validate(restRamlWithTitleVersion()
+                .withBaseUri("http://localhost:8080/warname/query/view/rest/service").build());
     }
 
     @Test
     public void shouldPassIfBaseUriContainsTheCommandPillarAndApiTier() throws Exception {
-        validator.validate(new RamlBuilder()
-                .withVersion("#%RAML 0.8")
-                .withTitle("Example Service")
-                .withBaseUri("http://localhost:8080/warname/command/api/rest/service")
-                .build());
+        validator.validate(restRamlWithTitleVersion()
+                .withBaseUri("http://localhost:8080/warname/command/api/rest/service").build());
     }
 
     @Test
     public void shouldPassIfBaseUriContainsTheCommandPillarAndControllerTier() throws Exception {
-        validator.validate(new RamlBuilder()
-                .withVersion("#%RAML 0.8")
-                .withTitle("Example Service")
-                .withBaseUri("http://localhost:8080/warname/command/controller/rest/service")
-                .build());
+        validator.validate(restRamlWithTitleVersion()
+                .withBaseUri("http://localhost:8080/warname/command/controller/rest/service").build());
     }
 
     @Test
     public void shouldPassIfBaseUriContainsTheCommandPillarAndHandlerTier() throws Exception {
-        validator.validate(new RamlBuilder()
-                .withVersion("#%RAML 0.8")
-                .withTitle("Example Service")
-                .withBaseUri("http://localhost:8080/warname/command/handler/rest/service")
-                .build());
+        validator.validate(restRamlWithTitleVersion()
+                .withBaseUri("http://localhost:8080/warname/command/handler/rest/service").build());
     }
 
     @Test
@@ -73,11 +57,8 @@ public class BaseUriRamlValidatorTest {
         exception.expect(RamlValidationException.class);
         exception.expectMessage("Pillar and tier of service should be specified in the base uri: http://localhost:8080/warname/api/rest/service");
 
-        validator.validate(new RamlBuilder()
-                .withVersion("#%RAML 0.8")
-                .withTitle("Example Service")
-                .withBaseUri("http://localhost:8080/warname/api/rest/service")
-                .build());
+        validator.validate(restRamlWithTitleVersion()
+                .withBaseUri("http://localhost:8080/warname/api/rest/service").build());
     }
 
     @Test
@@ -85,11 +66,8 @@ public class BaseUriRamlValidatorTest {
         exception.expect(RamlValidationException.class);
         exception.expectMessage("Pillar and tier of service should be specified in the base uri: http://localhost:8080/warname/command/rest/service");
 
-        validator.validate(new RamlBuilder()
-                .withVersion("#%RAML 0.8")
-                .withTitle("Example Service")
-                .withBaseUri("http://localhost:8080/warname/command/rest/service")
-                .build());
+        validator.validate(restRamlWithTitleVersion()
+                .withBaseUri("http://localhost:8080/warname/command/rest/service").build());
     }
 
     @Test
@@ -97,11 +75,8 @@ public class BaseUriRamlValidatorTest {
         exception.expect(RamlValidationException.class);
         exception.expectMessage("Pillar and tier of service should be specified in the base uri: http://localhost:8080/warname/event/listener/rest/service");
 
-        validator.validate(new RamlBuilder()
-                .withVersion("#%RAML 0.8")
-                .withTitle("Example Service")
-                .withBaseUri("http://localhost:8080/warname/event/listener/rest/service")
-                .build());
+        validator.validate(restRamlWithTitleVersion()
+                .withBaseUri("http://localhost:8080/warname/event/listener/rest/service").build());
     }
 
 }

CJSCommonPlatform/microservice_framework_version_6

generators/rest-adapter-generator/src/test/java/uk/gov/justice/services/adapters/rest/validator/BaseUriRamlValidatorTest.java
@@ -1,8 +1,9 @@
 package uk.gov.justice.services.adapters.rest.validator;
 
+import static uk.gov.justice.services.generators.test.utils.builder.RamlBuilder.restRamlWithTitleVersion;
+
 import uk.gov.justice.services.generators.commons.validator.RamlValidationException;
 import uk.gov.justice.services.generators.commons.validator.RamlValidator;
-import uk.gov.justice.services.generators.test.utils.builder.RamlBuilder;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -12,60 +13,43 @@ public class BaseUriRamlValidatorTest {
 
     @Rule
     public ExpectedException exception = ExpectedException.none();
+
     private RamlValidator validator = new BaseUriRamlValidator();
 
     @Test
     public void shouldPassIfBaseUriContainsTheQueryPillarAndApiTier() throws Exception {
-        validator.validate(new RamlBuilder()
-                .withVersion("#%RAML 0.8")
-                .withTitle("Example Service")
-                .withBaseUri("http://localhost:8080/warname/query/api/rest/service")
-                .build());
+        validator.validate(restRamlWithTitleVersion()
+                .withBaseUri("http://localhost:8080/warname/query/api/rest/service").build());
     }
 
     @Test
     public void shouldPassIfBaseUriContainsTheQueryPillarAndControllerTier() throws Exception {
-        validator.validate(new RamlBuilder()
-                .withVersion("#%RAML 0.8")
-                .withTitle("Example Service")
-                .withBaseUri("http://localhost:8080/warname/query/controller/rest/service")
-                .build());
+        validator.validate(restRamlWithTitleVersion()
+                .withBaseUri("http://localhost:8080/warname/query/controller/rest/service").build());
     }
 
     @Test
     public void shouldPassIfBaseUriContainsTheQueryPillarAndViewTier() throws Exception {
-        validator.validate(new RamlBuilder()
-                .withVersion("#%RAML 0.8")
-                .withTitle("Example Service")
-                .withBaseUri("http://localhost:8080/warname/query/view/rest/service")
-                .build());
+        validator.validate(restRamlWithTitleVersion()
+                .withBaseUri("http://localhost:8080/warname/query/view/rest/service").build());
     }
 
     @Test
     public void shouldPassIfBaseUriContainsTheCommandPillarAndApiTier() throws Exception {
-        validator.validate(new RamlBuilder()
-                .withVersion("#%RAML 0.8")
-                .withTitle("Example Service")
-                .withBaseUri("http://localhost:8080/warname/command/api/rest/service")
-                .build());
+        validator.validate(restRamlWithTitleVersion()
+                .withBaseUri("http://localhost:8080/warname/command/api/rest/service").build());
     }
 
     @Test
     public void shouldPassIfBaseUriContainsTheCommandPillarAndControllerTier() throws Exception {
-        validator.validate(new RamlBuilder()
-                .withVersion("#%RAML 0.8")
-                .withTitle("Example Service")
-                .withBaseUri("http://localhost:8080/warname/command/controller/rest/service")
-                .build());
+        validator.validate(restRamlWithTitleVersion()
+                .withBaseUri("http://localhost:8080/warname/command/controller/rest/service").build());
     }
 
     @Test
     public void shouldPassIfBaseUriContainsTheCommandPillarAndHandlerTier() throws Exception {
-        validator.validate(new RamlBuilder()
-                .withVersion("#%RAML 0.8")
-                .withTitle("Example Service")
-                .withBaseUri("http://localhost:8080/warname/command/handler/rest/service")
-                .build());
+        validator.validate(restRamlWithTitleVersion()
+                .withBaseUri("http://localhost:8080/warname/command/handler/rest/service").build());
     }
 
     @Test
@@ -73,11 +57,8 @@ public class BaseUriRamlValidatorTest {
         exception.expect(RamlValidationException.class);
         exception.expectMessage("Pillar and tier of service should be specified in the base uri: http://localhost:8080/warname/api/rest/service");
 
-        validator.validate(new RamlBuilder()
-                .withVersion("#%RAML 0.8")
-                .withTitle("Example Service")
-                .withBaseUri("http://localhost:8080/warname/api/rest/service")
-                .build());
+        validator.validate(restRamlWithTitleVersion()
+                .withBaseUri("http://localhost:8080/warname/api/rest/service").build());
     }
 
     @Test
@@ -85,11 +66,8 @@ public class BaseUriRamlValidatorTest {
         exception.expect(RamlValidationException.class);
         exception.expectMessage("Pillar and tier of service should be specified in the base uri: http://localhost:8080/warname/command/rest/service");
 
-        validator.validate(new RamlBuilder()
-                .withVersion("#%RAML 0.8")
-                .withTitle("Example Service")
-                .withBaseUri("http://localhost:8080/warname/command/rest/service")
-                .build());
+        validator.validate(restRamlWithTitleVersion()
+                .withBaseUri("http://localhost:8080/warname/command/rest/service").build());
     }
 
     @Test
@@ -97,11 +75,8 @@ public class BaseUriRamlValidatorTest {
         exception.expect(RamlValidationException.class);
         exception.expectMessage("Pillar and tier of service should be specified in the base uri: http://localhost:8080/warname/event/listener/rest/service");
 
-        validator.validate(new RamlBuilder()
-                .withVersion("#%RAML 0.8")
-                .withTitle("Example Service")
-                .withBaseUri("http://localhost:8080/warname/event/listener/rest/service")
-                .build());
+        validator.validate(restRamlWithTitleVersion()
+                .withBaseUri("http://localhost:8080/warname/event/listener/rest/service").build());
     }
 
 }
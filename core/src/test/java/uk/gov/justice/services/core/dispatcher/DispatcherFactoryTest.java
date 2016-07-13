60b0d453266d68e3bc0c2c15cfaf3e6dbc7c9e1e
David Edwards <david@more.fool.me.uk>
2016-07-13 12:24:04 +0100

    Only do access control check for local handlers

--- core/src/test/java/uk/gov/justice/services/core/dispatcher/DispatcherFactoryTest.java
+++ core/src/test/java/uk/gov/justice/services/core/dispatcher/DispatcherFactoryTest.java
@@ -5,11 +5,16 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.junit.Assert.assertThat;
+import static uk.gov.justice.services.core.annotation.ServiceComponentLocation.LOCAL;
+import static uk.gov.justice.services.core.annotation.ServiceComponentLocation.REMOTE;
 
 import uk.gov.justice.services.core.accesscontrol.AccessControlFailureMessageGenerator;
 import uk.gov.justice.services.core.accesscontrol.AccessControlService;
 import uk.gov.justice.services.core.handler.registry.HandlerRegistry;
 
+import java.util.Optional;
+
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -33,26 +38,35 @@ public class DispatcherFactoryTest {
     private DispatcherFactory dispatcherFactory;
 
     @Test
-    public void shouldCreateANewDispatcherUsingTheInjectedDependencies() throws Exception {
+    public void shouldCreateDispatcherUsingTheInjectedDependencies() throws Exception {
 
-        final Dispatcher dispatcher = dispatcherFactory.createNew();
-        final AccessControlService accessControlServiceFromClass = getAccessControlServiceFrom(dispatcher);
+        final Dispatcher dispatcher = dispatcherFactory.createNew(LOCAL);
+        final Optional<AccessControlService> accessControlServiceFromClass = getAccessControlServiceFrom(dispatcher);
         final AccessControlFailureMessageGenerator accessControlFailureMessageGeneratorFromClass = getFailureMessageGeneratorFrom(dispatcher);
 
-        assertThat(accessControlServiceFromClass, is(sameInstance(accessControlService)));
+        assertThat(accessControlServiceFromClass.get(), is(sameInstance(accessControlService)));
         assertThat(accessControlFailureMessageGeneratorFromClass, is(sameInstance(accessControlFailureMessageGenerator)));
+    }
+
+    @Test
+    public void shouldCreateDispatcherWithNoAccessControlForRemoteLocation() throws Exception {
+
+        final Dispatcher dispatcher = dispatcherFactory.createNew(REMOTE);
+        final Optional<AccessControlService> accessControlServiceFromClass = getAccessControlServiceFrom(dispatcher);
+
+        assertThat(accessControlServiceFromClass.isPresent(), is(false));
     }
 
     @Test
     public void shouldCreateANewHandlerRegistryForEachDispatcherInstance() throws Exception {
 
-        final Dispatcher dispatcher_1 = dispatcherFactory.createNew();
-        final Dispatcher dispatcher_2 = dispatcherFactory.createNew();
+        final Dispatcher dispatcher1 = dispatcherFactory.createNew(LOCAL);
+        final Dispatcher dispatcher2 = dispatcherFactory.createNew(LOCAL);
 
-        final HandlerRegistry handlerRegistry_1 = getHandlerRegistryFrom(dispatcher_1);
-        final HandlerRegistry handlerRegistry_2 = getHandlerRegistryFrom(dispatcher_2);
+        final HandlerRegistry handlerRegistry1 = getHandlerRegistryFrom(dispatcher1);
+        final HandlerRegistry handlerRegistry2 = getHandlerRegistryFrom(dispatcher2);
 
-        assertThat(handlerRegistry_1, is(not(sameInstance(handlerRegistry_2))));
+        assertThat(handlerRegistry1, is(not(sameInstance(handlerRegistry2))));
     }
 
     private HandlerRegistry getHandlerRegistryFrom(final Dispatcher dispatcher_1) throws IllegalAccessException {
@@ -63,7 +77,7 @@ public class DispatcherFactoryTest {
         return (AccessControlFailureMessageGenerator) readField(dispatcher, FAILURE_MESSAGE_GENERATOR_FIELD_NAME, true);
     }
 
-    private AccessControlService getAccessControlServiceFrom(final Dispatcher dispatcher) throws IllegalAccessException {
-        return (AccessControlService) readField(dispatcher, ACCESS_CONTROL_SERVICE_FIELD_NAME, true);
+    private Optional<AccessControlService> getAccessControlServiceFrom(final Dispatcher dispatcher) throws IllegalAccessException {
+        return (Optional<AccessControlService>) readField(dispatcher, ACCESS_CONTROL_SERVICE_FIELD_NAME, true);
     }
 }

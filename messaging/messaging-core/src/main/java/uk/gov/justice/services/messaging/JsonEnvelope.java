c454cf02949bc5f944bb1cd155bb41e3a8af24ea
amckenzie <allan.mckenzie@agilesphere.co.uk>
2016-09-07 13:02:37 +0100

    comment the toDebugStringPrettyPrint() method on JsonEnvelope in the hope that people should use it correctly

--- messaging/messaging-core/src/main/java/uk/gov/justice/services/messaging/JsonEnvelope.java
+++ messaging/messaging-core/src/main/java/uk/gov/justice/services/messaging/JsonEnvelope.java
@@ -21,6 +21,27 @@ public interface JsonEnvelope extends Envelope<JsonValue> {
 
     JsonString payloadAsJsonString();
 
+    /**
+     * Pretty prints the actual json of this envelope.
+     *
+     * N.B. This should NEVER be used in production code as the json
+     * may contain sensitive data which can be harmful should it appear
+     * in logs.
+     *
+     * If you want to log this envelope for tracing purposes
+     * then use the toString() method, as this will give the
+     * objects metadata without exposing anything sensistive
+     *
+     * @return this envelope as it's json string, pretty printed.
+     **/
     String toDebugStringPrettyPrint();
 
+
+    /**
+     * Prints the json for logging purposes. Removes any potentially sensitive
+     * data.
+     *
+     * @return a json String of the envelope
+     */
+    String toString();
 }

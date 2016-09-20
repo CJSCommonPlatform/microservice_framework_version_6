cca665c6408aa42dbfa92346ed2c1f23a6677f4b
amckenzie <allan.mckenzie@agilesphere.co.uk>
2016-09-20 14:11:08 +0100

    add javadoc

--- test-utils/test-utils-core/src/main/java/uk/gov/justice/services/test/utils/core/http/PollingRequestParamsBuilder.java
+++ test-utils/test-utils-core/src/main/java/uk/gov/justice/services/test/utils/core/http/PollingRequestParamsBuilder.java
@@ -11,6 +11,10 @@ import javax.ws.rs.core.MultivaluedHashMap;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 
+/**
+ * Builder for creating PollingRequestParameters. Expects a url and a media type. All other
+ * required parameters for polling a request point are given as defaults.
+ */
 public class PollingRequestParamsBuilder {
 
     private final String url;
@@ -22,6 +26,13 @@ public class PollingRequestParamsBuilder {
     private long delayInMillis = DEFAULT_DELAY_MILLIS;
     private int retryCount = DEFAULT_RETRY_COUNT;
 
+    /**
+     * Convenience method for creating a new PollingRequestParamsBuilder
+     * @param url the url of the rest endpoint
+     * @param mediaType the media type
+     *
+     * @return a new PollingRequestParamsBuilder
+     */
     public static PollingRequestParamsBuilder pollingRequestParams(final String url, final String mediaType) {
         return new PollingRequestParamsBuilder(url, mediaType);
     }
@@ -31,41 +42,105 @@ public class PollingRequestParamsBuilder {
         this.mediaType = mediaType;
     }
 
-    public PollingRequestParamsBuilder withResponseCondition(final Predicate<Response> responseCondition) {
-        this.responseCondition = responseCondition;
-        return this;
-    }
-
-    public PollingRequestParamsBuilder withResultCondition(final Predicate<String> resultCondition) {
-        this.resultCondition = resultCondition;
-        return this;
-    }
-
+    /**
+     * Overrides the delay between polls. If not specified a default of 1 second is used.
+     * @param delayInMillis the delay betweeen polls in milliseconds
+     * @return this
+     */
     public PollingRequestParamsBuilder withDelayInMillis(final long delayInMillis) {
         this.delayInMillis = delayInMillis;
         return this;
     }
 
+    /**
+     * Overides the number of retries. If not specifed a default of 5 times is used.
+     * @param retryCount the number of retries
+     * @return this
+     */
     public PollingRequestParamsBuilder withRetryCount(final int retryCount) {
         this.retryCount = retryCount;
         return this;
     }
 
+    /**
+     * Adds a header to the rest request.
+     * @param name the name of the header
+     * @param value the value of the header
+     * @return this
+     */
     public PollingRequestParamsBuilder withHeader(final String name, final Object value) {
         headers.put(name, singletonList(value));
         return this;
     }
 
+    /**
+     * Adds a map of headers to the rest request.
+     * @param headers a map of header names to header values
+     * @return this
+     */
     public PollingRequestParamsBuilder withHeaders(final Map<String, Object> headers) {
-        this.headers = new MultivaluedHashMap<>(headers);
+        this.headers.putAll(new MultivaluedHashMap<>(headers));
         return this;
     }
 
+    /**
+     * Adds a condition for verifying the Response from the rest endpoint. By default allows all.
+     *
+     * To use:
+     *      <pre><blockquote>
+     *
+     *      new PollingRequestParamsBuilder()
+     *          .withResponseCondition(response -> response.getStatus() == 200)
+     *          .build();
+     *
+     *      </blockquote></pre>
+     *
+     * @param responseCondition a Predicate for verifying the rest Response
+     * @return this
+     */
+    public PollingRequestParamsBuilder withResponseCondition(final Predicate<Response> responseCondition) {
+        this.responseCondition = responseCondition;
+        return this;
+    }
+
+    /**
+     * Adds a condition for verifying the response body from a call to a rest endpoint. By default allows all.
+     *
+     * To use:
+     *      <pre><blockquote>
+     *
+     *      new PollingRequestParamsBuilder()
+     *          .withResultCondition(json -> json.equals("my-json"))
+     *          .build();
+     *
+     *      </blockquote></pre>
+     *
+     * @param resultCondition
+     * @return
+     */
+    public PollingRequestParamsBuilder withResultCondition(final Predicate<String> resultCondition) {
+        this.resultCondition = resultCondition;
+        return this;
+    }
+
+    /**
+     * Adds a pre configured result condition, which verified that all of the names/values exist
+     * in the response body json
+     *
+     * @See ExpectedJsonValuesResultCondition
+     *
+     * @param values a Map of property names to values that should exist in the result json
+     * @return this
+     */
     public PollingRequestParamsBuilder withExpectedJsonResponseValues(final Map<String, String> values) {
         this.resultCondition = new ExpectedJsonValuesResultCondition(values);
         return this;
     }
 
+    /**
+     * Builds PollingRequestParameters from the supplied values/defaults
+     * @return PollingRequestParameters
+     */
     public PollingRequestParams build() {
         return new PollingRequestParams(
                 url,
@@ -78,6 +153,11 @@ public class PollingRequestParamsBuilder {
         );
     }
 
+    /**
+     * Default condition to allow all in verification. Used as a default for the
+     * Response condition and the result condition.
+     * @param <T> either Response or String
+     */
     private static class DefaultCondition<T> implements Predicate<T> {
 
         @Override

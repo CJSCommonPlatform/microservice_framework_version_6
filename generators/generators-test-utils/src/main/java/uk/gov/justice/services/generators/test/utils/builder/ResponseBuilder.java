4eeb17d6b9a63e8e58e22be62d159118830c694d
Shaun Francis <Shaun.Francis@hmcts.net>
2017-01-25 10:47:44 +0000

    Test generators with http accepted response with no body

--- generators/generators-test-utils/src/main/java/uk/gov/justice/services/generators/test/utils/builder/ResponseBuilder.java
+++ generators/generators-test-utils/src/main/java/uk/gov/justice/services/generators/test/utils/builder/ResponseBuilder.java
@@ -1,11 +1,38 @@
 package uk.gov.justice.services.generators.test.utils.builder;
 
+import static java.util.Arrays.stream;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.raml.model.MimeType;
 import org.raml.model.Response;
 
 public class ResponseBuilder {
-    public static Response response(final String description) {
+
+    private final Map<String, MimeType> responseBody = new HashMap<>();
+    private String description = "";
+
+    public static ResponseBuilder response() {
+        return new ResponseBuilder();
+    }
+
+    public ResponseBuilder withDescriptionOf(final String description) {
+        this.description = description;
+        return this;
+    }
+
+    public ResponseBuilder withBodyTypes(final String... responseTypes) {
+        stream(responseTypes).forEach(type ->
+                this.responseBody.put(type, new MimeType(type))
+        );
+        return this;
+    }
+
+    public Response build() {
         final Response response = new Response();
         response.setDescription(description);
+        response.setBody(responseBody);
         return response;
     }
 }

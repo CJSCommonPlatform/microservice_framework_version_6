Andrew8305/microservice_framework

Add support for multipart POST to framework

generators/rest-adapter-generator/src/main/java/uk/gov/justice/services/adapters/rest/generator/JaxRsInterfaceGenerator.java
@@ -9,6 +9,7 @@ import static javax.lang.model.element.Modifier.PUBLIC;
 import static org.apache.commons.lang.StringUtils.defaultIfBlank;
 import static org.apache.commons.lang.StringUtils.strip;
 import static uk.gov.justice.services.adapters.rest.generator.Generators.byMimeTypeOrder;
+import static uk.gov.justice.services.adapters.rest.helper.Multiparts.isMultipartResource;
 import static uk.gov.justice.services.generators.commons.helper.Actions.responseMimeTypesOf;
 import static uk.gov.justice.services.generators.commons.helper.Names.DEFAULT_ANNOTATION_PARAMETER;
 import static uk.gov.justice.services.generators.commons.helper.Names.GENERIC_PAYLOAD_ARGUMENT_NAME;
@@ -35,6 +36,8 @@ import com.squareup.javapoet.CodeBlock;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.ParameterSpec;
 import com.squareup.javapoet.TypeSpec;
+import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartInput;
 import org.raml.model.Action;
 import org.raml.model.ActionType;
 import org.raml.model.MimeType;
@@ -49,6 +52,7 @@ import org.raml.model.parameter.UriParameter;
 class JaxRsInterfaceGenerator {
 
     private static final String ANNOTATION_FORMAT = "$S";
+    private static final String MULTIPART_INPUT = "multipartInput";
 
     /**
      * Generate Java code for a Raml structure
@@ -160,11 +164,17 @@ class JaxRsInterfaceGenerator {
                         .addMember(DEFAULT_ANNOTATION_PARAMETER, ANNOTATION_FORMAT, bodyMimeType.getType())
                         .build());
 
-        if (bodyMimeType.getSchema() != null) {
+        if (isMultipartResource(bodyMimeType)) {
+            methodBuilder.addParameter(ParameterSpec
+                    .builder(MultipartInput.class, MULTIPART_INPUT)
+                    .addAnnotation(MultipartForm.class)
+                    .build());
+        } else if (bodyMimeType.getSchema() != null) {
             methodBuilder.addParameter(ParameterSpec
                     .builder(JsonObject.class, GENERIC_PAYLOAD_ARGUMENT_NAME)
                     .build());
         }
+
         return methodBuilder;
     }
 

CJSCommonPlatform/microservice_framework_version_6

generators/rest-adapter-generator/src/main/java/uk/gov/justice/services/adapters/rest/generator/JaxRsInterfaceGenerator.java
@@ -9,6 +9,7 @@ import static javax.lang.model.element.Modifier.PUBLIC;
 import static org.apache.commons.lang.StringUtils.defaultIfBlank;
 import static org.apache.commons.lang.StringUtils.strip;
 import static uk.gov.justice.services.adapters.rest.generator.Generators.byMimeTypeOrder;
+import static uk.gov.justice.services.adapters.rest.helper.Multiparts.isMultipartResource;
 import static uk.gov.justice.services.generators.commons.helper.Actions.responseMimeTypesOf;
 import static uk.gov.justice.services.generators.commons.helper.Names.DEFAULT_ANNOTATION_PARAMETER;
 import static uk.gov.justice.services.generators.commons.helper.Names.GENERIC_PAYLOAD_ARGUMENT_NAME;
@@ -35,6 +36,8 @@ import com.squareup.javapoet.CodeBlock;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.ParameterSpec;
 import com.squareup.javapoet.TypeSpec;
+import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 import org.raml.model.Action;
 import org.raml.model.ActionType;
 import org.raml.model.MimeType;
@@ -49,6 +52,7 @@ import org.raml.model.parameter.UriParameter;
 class JaxRsInterfaceGenerator {
 
     private static final String ANNOTATION_FORMAT = "$S";
+    private static final String MULTIPART_FORM_DATA_INPUT = "multipartFormDataInput";
 
     /**
      * Generate Java code for a Raml structure
@@ -160,11 +164,17 @@ class JaxRsInterfaceGenerator {
                         .addMember(DEFAULT_ANNOTATION_PARAMETER, ANNOTATION_FORMAT, bodyMimeType.getType())
                         .build());
 
-        if (bodyMimeType.getSchema() != null) {
+        if (isMultipartResource(bodyMimeType)) {
+            methodBuilder.addParameter(ParameterSpec
+                    .builder(MultipartFormDataInput.class, MULTIPART_FORM_DATA_INPUT)
+                    .addAnnotation(MultipartForm.class)
+                    .build());
+        } else if (bodyMimeType.getSchema() != null) {
             methodBuilder.addParameter(ParameterSpec
                     .builder(JsonObject.class, GENERIC_PAYLOAD_ARGUMENT_NAME)
                     .build());
         }
+
         return methodBuilder;
     }
 
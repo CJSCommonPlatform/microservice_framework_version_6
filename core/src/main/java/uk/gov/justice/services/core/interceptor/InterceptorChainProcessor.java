550dba5266e7afacf53b180521cdfe63d51e8b4f
Shaun Francis <Shaun.Francis@hmcts.net>
2017-03-17 12:29:40 +0000

    Make Interceptor Chain Processor backwards compatible

--- core/src/main/java/uk/gov/justice/services/core/interceptor/InterceptorChainProcessor.java
+++ core/src/main/java/uk/gov/justice/services/core/interceptor/InterceptorChainProcessor.java
@@ -5,11 +5,12 @@ import uk.gov.justice.services.messaging.JsonEnvelope;
 import java.util.Optional;
 
 /**
- * Functional interface for the chain process this represents both asynchronous and synchronous
- * function calls.
+ * Interface to start processing an InterceptorContext or JsonEnvelope.
  */
-@FunctionalInterface
 public interface InterceptorChainProcessor {
 
     Optional<JsonEnvelope> process(final InterceptorContext interceptorContext);
+
+    @Deprecated
+    Optional<JsonEnvelope> process(final JsonEnvelope jsonEnvelope);
 }
--- core/src/main/java/uk/gov/justice/services/core/interceptor/InterceptorChainProcessorProducer.java
+++ core/src/main/java/uk/gov/justice/services/core/interceptor/InterceptorChainProcessorProducer.java
@@ -1,13 +1,9 @@
 package uk.gov.justice.services.core.interceptor;
 
 import static java.lang.String.format;
-import static uk.gov.justice.services.core.interceptor.InterceptorContext.copyWithOutput;
 import static uk.gov.justice.services.messaging.logging.LoggerUtils.trace;
 
 import uk.gov.justice.services.core.dispatcher.DispatcherCache;
-import uk.gov.justice.services.messaging.JsonEnvelope;
-
-import java.util.function.Function;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
@@ -36,32 +32,8 @@ public class InterceptorChainProcessorProducer {
      */
     @Produces
     public InterceptorChainProcessor produceProcessor(final InjectionPoint injectionPoint) {
-        final Function<JsonEnvelope, JsonEnvelope> dispatch = dispatcherCache.dispatcherFor(injectionPoint)::dispatch;
-
         trace(logger, () -> format("Interceptor Chain Processor provided for %s", injectionPoint.getClass().getName()));
 
-        return createProcessor(dispatch);
-    }
-
-    /**
-     * Constructs the {@link InterceptorChainProcessor} for the given dispatch interceptor target
-     * and injection point.
-     *
-     * @param dispatch the dispatch target method
-     * @return the interceptor chain processor function
-     */
-    private InterceptorChainProcessor createProcessor(final Function<JsonEnvelope, JsonEnvelope> dispatch) {
-
-        return interceptorContext -> {
-            final InterceptorChain interceptorChain = new InterceptorChain(interceptorCache.getInterceptors(), targetOf(dispatch));
-
-            return interceptorChain
-                    .processNext(interceptorContext)
-                    .outputEnvelope();
-        };
-    }
-
-    private Target targetOf(final Function<JsonEnvelope, JsonEnvelope> dispatch) {
-        return interceptorContext -> copyWithOutput(interceptorContext, dispatch.apply(interceptorContext.inputEnvelope()));
+        return new DefaultInterceptorChainProcessor(interceptorCache, dispatcherCache.dispatcherFor(injectionPoint)::dispatch);
     }
 }

74dc062019c3b9dd71f771344d5ededdc0135f92
David Edwards <david@more.fool.me.uk>
2017-03-27 14:35:47 +0100

    Avoid race condition in event stream append
    Fixes #356 by using the maximum version number in the events that
    have been read instead of getting the version number from the database

--- event-sourcing/event-source/src/main/java/uk/gov/justice/services/eventsourcing/source/core/EnvelopeEventStream.java
+++ event-sourcing/event-source/src/main/java/uk/gov/justice/services/eventsourcing/source/core/EnvelopeEventStream.java
@@ -1,5 +1,7 @@
 package uk.gov.justice.services.eventsourcing.source.core;
 
+import static uk.gov.justice.services.eventsourcing.source.core.Tolerance.CONSECUTIVE;
+
 import uk.gov.justice.services.eventsourcing.source.core.exception.EventStreamException;
 import uk.gov.justice.services.messaging.JsonEnvelope;
 
@@ -10,7 +12,8 @@ public class EnvelopeEventStream implements EventStream {
 
     private final EventStreamManager eventStreamManager;
     private final UUID id;
-
+    private Long currentVersion;
+    private boolean isRead = false;
 
     EnvelopeEventStream(final UUID id, final EventStreamManager eventStreamManager) {
         this.id = id;
@@ -19,26 +22,28 @@ public class EnvelopeEventStream implements EventStream {
 
     @Override
     public Stream<JsonEnvelope> read() {
-        return eventStreamManager.read(id);
+        markAsReadFrom(0L);
+        return eventStreamManager.read(id).map(this::recordCurrentVersion);
     }
 
     @Override
     public Stream<JsonEnvelope> readFrom(final long version) {
-        return eventStreamManager.readFrom(id, version);
+        markAsReadFrom(version - 1);
+        return eventStreamManager.readFrom(id, version).map(this::recordCurrentVersion);
     }
 
     @Override
     public long append(final Stream<JsonEnvelope> events) throws EventStreamException {
-        return eventStreamManager.append(id, events);
+        return append(events, CONSECUTIVE);
     }
 
     @Override
-    public long append(final Stream<JsonEnvelope> stream, final Tolerance tolerance) throws EventStreamException {
+    public long append(final Stream<JsonEnvelope> events, final Tolerance tolerance) throws EventStreamException {
         switch (tolerance) {
             case NON_CONSECUTIVE:
-                return eventStreamManager.appendNonConsecutively(id, stream);
+                return eventStreamManager.appendNonConsecutively(id, events);
             default:
-                return eventStreamManager.append(id, stream);
+                return isRead ? eventStreamManager.appendAfter(id, events, currentVersion) : eventStreamManager.append(id, events);
         }
     }
 
@@ -49,11 +54,24 @@ public class EnvelopeEventStream implements EventStream {
 
     @Override
     public long getCurrentVersion() {
-        return eventStreamManager.getCurrentVersion(id);
+        return isRead ? currentVersion : eventStreamManager.getCurrentVersion(id);
     }
 
     @Override
     public UUID getId() {
         return id;
+    }
+
+    private JsonEnvelope recordCurrentVersion(final JsonEnvelope event) {
+        currentVersion = event.metadata().version().orElseThrow(() -> new IllegalStateException("Missing version in event from event store"));
+        return event;
+    }
+
+    private synchronized void markAsReadFrom(final long version) {
+        if (isRead) {
+            throw new IllegalStateException("Event stream has already been read");
+        }
+        isRead = true;
+        currentVersion = version;
     }
 }

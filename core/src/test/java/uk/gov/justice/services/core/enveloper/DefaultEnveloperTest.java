2d634b80b64c1d6de906cb372ccafbab0270392d
Jacek Obarymski <jacek.obarymski@agilesphere.co.uk>
2017-04-07 13:07:13 +0100

    Remove stream metadata when creating new envelope

--- core/src/test/java/uk/gov/justice/services/core/enveloper/DefaultEnveloperTest.java
+++ core/src/test/java/uk/gov/justice/services/core/enveloper/DefaultEnveloperTest.java
@@ -1,25 +1,26 @@
 package uk.gov.justice.services.core.enveloper;
 
+import static java.util.Optional.empty;
+import static java.util.UUID.randomUUID;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.hamcrest.core.IsNot.not;
+import static uk.gov.justice.services.messaging.DefaultJsonEnvelope.envelope;
 import static uk.gov.justice.services.messaging.JsonObjectMetadata.metadataOf;
+import static uk.gov.justice.services.messaging.JsonObjectMetadata.metadataWithDefaults;
 
 import uk.gov.justice.domain.annotation.Event;
 import uk.gov.justice.services.common.converter.ObjectToJsonValueConverter;
-import uk.gov.justice.services.common.util.Clock;
+import uk.gov.justice.services.common.converter.jackson.ObjectMapperProducer;
+import uk.gov.justice.services.common.util.UtcClock;
 import uk.gov.justice.services.core.enveloper.exception.InvalidEventException;
 import uk.gov.justice.services.core.extension.EventFoundEvent;
 import uk.gov.justice.services.messaging.JsonEnvelope;
 
-import java.time.ZonedDateTime;
 import java.util.UUID;
 
-import javax.json.JsonObject;
 import javax.json.JsonValue;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -28,104 +29,80 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DefaultEnveloperTest {
 
     private static final String TEST_EVENT_NAME = "test.event.something-happened";
-    private static final UUID COMMAND_UUID = UUID.randomUUID();
-    private static final UUID OLD_CAUSATION_ID = UUID.randomUUID();
+    private static final UUID COMMAND_UUID = randomUUID();
+    private static final UUID OLD_CAUSATION_ID = randomUUID();
     private static final String TEST_NAME = "test.query.query-response";
 
     @Rule
     public ExpectedException exception = ExpectedException.none();
 
-    @InjectMocks
     private DefaultEnveloper enveloper;
-
-    private Object object;
-
-    @Mock
-    private ObjectToJsonValueConverter objectToJsonValueConverter;
-
-    @Mock
-    private JsonEnvelope envelope;
-
-    @Mock
-    private EventFoundEvent event;
-
-    @Mock
-    private JsonObject payload;
-
-    @Mock
-    private Clock clock;
 
     @Before
     public void setup() throws JsonProcessingException {
-        object = new TestEvent();
-
-        doReturn(TestEvent.class).when(event).getClazz();
-        when(event.getEventName()).thenReturn(TEST_EVENT_NAME);
+        enveloper = new DefaultEnveloper(
+                new UtcClock(),
+                new ObjectToJsonValueConverter(new ObjectMapperProducer().objectMapper()));
     }
 
     @Test
     public void shouldEnvelopeEventObject() throws JsonProcessingException {
-        enveloper.register(event);
-        when(envelope.metadata()).thenReturn(
-                metadataOf(COMMAND_UUID, TEST_EVENT_NAME)
-                        .withCausation(OLD_CAUSATION_ID)
-                        .build());
-        when(objectToJsonValueConverter.convert(object)).thenReturn(payload);
-        when(clock.now()).thenReturn(ZonedDateTime.now());
+        enveloper.register(new EventFoundEvent(TestEvent.class, TEST_EVENT_NAME));
 
-        JsonEnvelope event = enveloper.withMetadataFrom(envelope).apply(object);
+        JsonEnvelope event = enveloper.withMetadataFrom(
+                envelope()
+                        .with(metadataOf(COMMAND_UUID, TEST_EVENT_NAME)
+                                .withCausation(OLD_CAUSATION_ID))
+                        .build())
+                .apply(new TestEvent("somePayloadValue"));
 
-        assertThat(event.payloadAsJsonObject(), equalTo(payload));
         assertThat(event.metadata().id(), notNullValue());
+        assertThat(event.metadata().id(), not(equalTo(COMMAND_UUID)));
         assertThat(event.metadata().name(), equalTo(TEST_EVENT_NAME));
         assertThat(event.metadata().causation().size(), equalTo(2));
         assertThat(event.metadata().causation().get(0), equalTo(OLD_CAUSATION_ID));
         assertThat(event.metadata().causation().get(1), equalTo(COMMAND_UUID));
-        verify(objectToJsonValueConverter, times(1)).convert(object);
+        assertThat(event.payloadAsJsonObject().getString("somePayloadKey"), equalTo("somePayloadValue"));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void shouldThrowExceptionOnNullEvent() throws JsonProcessingException {
-        enveloper.withMetadataFrom(envelope).apply(null);
+        enveloper.withMetadataFrom(envelope().build()).apply(null);
     }
 
     @Test
     public void shouldEnvelopeObjectWithName() throws JsonProcessingException {
-        when(envelope.metadata()).thenReturn(
-                metadataOf(COMMAND_UUID, TEST_NAME)
-                        .withCausation(OLD_CAUSATION_ID)
-                        .build());
-        when(objectToJsonValueConverter.convert(object)).thenReturn(payload);
-        when(clock.now()).thenReturn(ZonedDateTime.now());
 
-        JsonEnvelope event = enveloper.withMetadataFrom(envelope, TEST_NAME).apply(object);
+        JsonEnvelope event = enveloper.withMetadataFrom(
+                envelope()
+                        .with(metadataOf(COMMAND_UUID, TEST_EVENT_NAME)
+                                .withCausation(OLD_CAUSATION_ID))
+                        .build(), TEST_NAME)
+                .apply(new TestEvent());
 
-        assertThat(event.payload(), equalTo(payload));
+
         assertThat(event.metadata().id(), notNullValue());
         assertThat(event.metadata().name(), equalTo(TEST_NAME));
         assertThat(event.metadata().causation().size(), equalTo(2));
         assertThat(event.metadata().causation().get(0), equalTo(OLD_CAUSATION_ID));
         assertThat(event.metadata().causation().get(1), equalTo(COMMAND_UUID));
-        verify(objectToJsonValueConverter, times(1)).convert(object);
     }
 
     @Test
     public void shouldEnvelopeMapNullObjectWithName() throws JsonProcessingException {
-        when(envelope.metadata()).thenReturn(
-                metadataOf(COMMAND_UUID, TEST_NAME)
-                        .withCausation(OLD_CAUSATION_ID)
-                        .build());
-        when(clock.now()).thenReturn(ZonedDateTime.now());
 
-        JsonEnvelope event = enveloper.withMetadataFrom(envelope, TEST_NAME).apply(null);
+        JsonEnvelope event = enveloper.withMetadataFrom(
+                envelope()
+                        .with(metadataOf(COMMAND_UUID, TEST_EVENT_NAME)
+                                .withCausation(OLD_CAUSATION_ID))
+                        .build(), TEST_NAME)
+                .apply(null);
 
         assertThat(event.payload(), equalTo(JsonValue.NULL));
         assertThat(event.metadata().id(), notNullValue());
@@ -133,26 +110,24 @@ public class DefaultEnveloperTest {
         assertThat(event.metadata().causation().size(), equalTo(2));
         assertThat(event.metadata().causation().get(0), equalTo(OLD_CAUSATION_ID));
         assertThat(event.metadata().causation().get(1), equalTo(COMMAND_UUID));
-        verify(objectToJsonValueConverter, times(0)).convert(object);
     }
 
     @Test
     public void shouldEnvelopeObjectWithoutCausation() throws JsonProcessingException {
-        enveloper.register(event);
-        when(envelope.metadata()).thenReturn(
-                metadataOf(COMMAND_UUID, TEST_EVENT_NAME)
-                        .build());
-        when(objectToJsonValueConverter.convert(object)).thenReturn(payload);
-        when(clock.now()).thenReturn(ZonedDateTime.now());
+        enveloper.register(new EventFoundEvent(TestEvent.class, TEST_EVENT_NAME));
 
-        JsonEnvelope event = enveloper.withMetadataFrom(envelope).apply(object);
+        JsonEnvelope event = enveloper.withMetadataFrom(
+                envelope()
+                        .with(metadataOf(COMMAND_UUID, TEST_EVENT_NAME))
+                        .build())
+                .apply(new TestEvent());
 
-        assertThat(event.payloadAsJsonObject(), equalTo(payload));
+
         assertThat(event.metadata().id(), notNullValue());
         assertThat(event.metadata().name(), equalTo(TEST_EVENT_NAME));
         assertThat(event.metadata().causation().size(), equalTo(1));
         assertThat(event.metadata().causation().get(0), equalTo(COMMAND_UUID));
-        verify(objectToJsonValueConverter, times(1)).convert(object);
+
     }
 
     @Test
@@ -160,10 +135,39 @@ public class DefaultEnveloperTest {
         exception.expect(InvalidEventException.class);
         exception.expectMessage("Failed to map event. No event registered for class java.lang.String");
 
-        enveloper.withMetadataFrom(envelope).apply("InvalidEventObject");
+        enveloper.withMetadataFrom(envelope().build()).apply("InvalidEventObject");
+    }
+
+    @Test
+    public void shouldRemoveStreamMetadata() throws JsonProcessingException {
+        enveloper.register(new EventFoundEvent(TestEvent.class, TEST_EVENT_NAME));
+
+        JsonEnvelope event = enveloper.withMetadataFrom(
+                envelope()
+                        .with(metadataWithDefaults()
+                                .withStreamId(randomUUID())
+                                .withVersion(123l)
+                        )
+                        .build())
+                .apply(new TestEvent());
+
+        assertThat(event.metadata().streamId(), is(empty()));
+        assertThat(event.metadata().version(), is(empty()));
     }
 
     @Event("Test-Event")
     public static class TestEvent {
+        private String somePayloadKey;
+
+        public TestEvent(final String somePayloadKey) {
+            this.somePayloadKey = somePayloadKey;
+        }
+
+        public TestEvent() {
+        }
+
+        public String getSomePayloadKey() {
+            return somePayloadKey;
+        }
     }
 }

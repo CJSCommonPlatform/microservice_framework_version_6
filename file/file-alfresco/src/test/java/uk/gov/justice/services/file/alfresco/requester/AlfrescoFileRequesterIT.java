354624fc62d57de1ae42177bfa004ce3d2e63f78
Krishana Sandrasegaram <ksandrasegaram@zaizi.com>
2017-05-04 13:05:52 +0100

    Revert "CRC-5397 - Call the new Alfresco API to Provide document as PDF - Review comments incorporated"
    This reverts commit 08c18eb706b875e9e7bbec52cf0991d762d940cb.

--- file/file-alfresco/src/test/java/uk/gov/justice/services/file/alfresco/requester/AlfrescoFileRequesterIT.java
+++ file/file-alfresco/src/test/java/uk/gov/justice/services/file/alfresco/requester/AlfrescoFileRequesterIT.java
@@ -10,14 +10,17 @@ import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 import static java.lang.String.format;
 import static java.util.UUID.randomUUID;
+
+import org.hamcrest.CoreMatchers;
+import org.jboss.resteasy.specimpl.MultivaluedMapImpl;
+
+import static javax.ws.rs.core.HttpHeaders.CONTENT_TYPE;
 import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
+import static javax.ws.rs.core.Response.Status.OK;
 import static org.apache.openejb.util.NetworkUtil.getNextAvailablePort;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.fail;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static uk.gov.justice.services.test.utils.common.reflection.ReflectionUtils.setField;
 
 import uk.gov.justice.services.file.alfresco.common.AlfrescoRestClient;
@@ -29,6 +32,7 @@ import java.io.InputStream;
 import java.util.Optional;
 
 import javax.ws.rs.ProcessingException;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.MultivaluedMap;
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
@@ -44,7 +48,7 @@ public class AlfrescoFileRequesterIT {
     private static final String UNUSED_MIME_TYPE = "text/plain";
     private static final String UNUSED_FILE_NAME = "file.txt";
     private static final String ALFRESCO_WORKSPACE_PATH = "/service/api/node/content/workspace/SpacesStore/";
-    private static final String ALFRESCO_WORKSPACE_PDF_TRANSFORMATION_PATH = "/service/api/requestpdf/workspace/SpacesStore/";
+    private static final String ALFRESCO_WORKSPACE_TRANSFORMATION_PATH = "service/api/node/workspace/SpacesStore/";
 
 
     private static int PORT = getNextAvailablePort();
@@ -53,7 +57,7 @@ public class AlfrescoFileRequesterIT {
 
     private RestClient restClient;
 
-    private MultivaluedMap<String, Object> headers;
+    private MultivaluedMap<String,Object> headers;
 
     @Rule
     public WireMockRule wireMock = new WireMockRule(PORT);
@@ -98,18 +102,44 @@ public class AlfrescoFileRequesterIT {
         final String fileId = randomUUID().toString();
         final String mimeType = "application/pdf";
         final String fileName = "file123.txt";
-        final String fileContent = "abcd";
 
-        stubFor(get(urlMatching(format("/alfresco%s%s/%s", ALFRESCO_WORKSPACE_PDF_TRANSFORMATION_PATH, fileId, fileName)))
-                .withHeader("cppuid", equalTo("user1234"))
-                .willReturn(aResponse().withHeader("Content-Type", mimeType).withBody(fileContent)));
+        restClient = new RestClient();
 
-        final Optional<InputStream> inputStream = fileRequester.requestPdf(fileId, fileName);
+        headers = new MultivaluedMapImpl<>();
+        headers.add("cppuid", "user1234");
+
+        final Response readMaterialResponseAsPdf = restClient.query(format(basePathWithPort(PORT) + "/%s%s?transformpdf=true", ALFRESCO_WORKSPACE_TRANSFORMATION_PATH, fileId), "*/*", headers);
+
+        assertThat(readMaterialResponseAsPdf.getStatus(), is(OK.getStatusCode()));
+        assertThat(readMaterialResponseAsPdf.getHeaderString(CONTENT_TYPE), CoreMatchers.equalTo("application/pdf;charset=UTF-8"));
+
+        final Optional<InputStream> inputStream = fileRequester.request(fileId, mimeType, fileName, true);
         assertTrue(inputStream.isPresent());
 
-        assertThat(IOUtils.toString(inputStream.get()), is(fileContent));
+        assertEquals(IOUtils.toString(inputStream.get()).length(), readMaterialResponseAsPdf.readEntity(String.class).length());
     }
 
+    @Test
+    public void shouldReturnOriginalContentFromAlfresco() throws IOException {
+        final String fileId = randomUUID().toString();
+        final String mimeType = "text/plain";
+        final String fileName = "file123.txt";
+
+        restClient = new RestClient();
+
+        headers = new MultivaluedMapImpl<>();
+        headers.add("cppuid", "user1234");
+
+        final Response readMaterialResponseAsPdf = restClient.query(format(basePathWithPort(PORT) + "/%s%s?transformpdf=false", ALFRESCO_WORKSPACE_TRANSFORMATION_PATH, fileId), "*/*", headers);
+
+        assertThat(readMaterialResponseAsPdf.getStatus(), is(OK.getStatusCode()));
+        assertThat(readMaterialResponseAsPdf.getHeaderString(CONTENT_TYPE), CoreMatchers.equalTo("text/plain;charset=UTF-8"));
+
+        final Optional<InputStream> inputStream = fileRequester.request(fileId, mimeType, fileName, false);
+        assertTrue(inputStream.isPresent());
+
+        assertEquals(IOUtils.toString(inputStream.get()).length(), readMaterialResponseAsPdf.readEntity(String.class).length());
+    }
 
     @Test
     public void shouldReturnOptionalEmptyFromAlfrescoIfFileIsNotFound() {
@@ -154,7 +184,7 @@ public class AlfrescoFileRequesterIT {
     private static AlfrescoFileRequester alfrescoFileRequesterWith(final String basePath) {
         AlfrescoFileRequester fileRequester = new AlfrescoFileRequester();
         fileRequester.alfrescoWorkspacePath = ALFRESCO_WORKSPACE_PATH;
-        fileRequester.alfrescoPdfContentWorkspacePath = ALFRESCO_WORKSPACE_PDF_TRANSFORMATION_PATH;
+        fileRequester.alfrescoPdfContentWorkspacePath = "/" + ALFRESCO_WORKSPACE_TRANSFORMATION_PATH;
         fileRequester.alfrescoReadUser = "user1234";
         fileRequester.restClient = new AlfrescoRestClient();
         setField(fileRequester.restClient, "alfrescoBaseUri", basePath);
--- file/file-api/src/main/java/uk/gov/justice/services/file/api/requester/FileRequester.java
+++ file/file-api/src/main/java/uk/gov/justice/services/file/api/requester/FileRequester.java
@@ -10,22 +10,21 @@ public interface FileRequester {
 
     /**
      * Requests a file from the FileService.
-     *
-     * @param fileId       - the unique id of the file.
+     * @param fileId - the unique id of the file.
      * @param fileMimeType - mime-type of the file.
-     * @param fileName     - name of the file.
+     * @param fileName - name of the file.
      * @return streamed content of the file.
      */
     Optional<InputStream> request(final String fileId, final String fileMimeType, final String fileName);
 
     /**
-     * Requests for stream the pdf transformation of the file that given by its fileId from the FileService.
-     *
-     * @param fileId   - the unique id of the file.
+     * Requests a file from the FileService.
+     * @param fileId - the unique id of the file.
      * @param fileName - name of the file.
+     * @param transformPdf - is pdf transformation of the file needed.
      * @return streamed content of the file.
      */
-    Optional<InputStream> requestPdf(final String fileId, final String fileName);
+    Optional<InputStream> request(final String fileId, final String fileMimeType, final String fileName, final boolean transformPdf);
 
 }
 

3cee85bc2b6a7580a5a2318d1ac0e194d2fe5c08
Joao Victor Ferreira <joao.ferreira@agilesphere.co.uk>
2017-08-22 13:05:10 +0100

    Created a factory for using an actual JsonObjectToObjectConverter in tests

---
+++ test-utils/test-utils-core/src/test/java/uk/gov/justice/services/test/utils/core/converter/JsonObjectToObjectConverterFactoryTest.java
@@ -0,0 +1,43 @@
+package uk.gov.justice.services.test.utils.core.converter;
+
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import uk.gov.justice.services.common.converter.JsonObjectToObjectConverter;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+
+import org.junit.Test;
+
+public class JsonObjectToObjectConverterFactoryTest {
+
+    @Test
+    public void testUsableJsonObjectToObjectConverterIsProduced(){
+        JsonObjectToObjectConverter converter = JsonObjectToObjectConverterFactory.createJsonObjectToObjectConverter();
+
+        assertThat(converter, is(notNullValue()));
+
+        JsonObject jsonObject = Json.createObjectBuilder()
+                .add("testAttribute", "testValue")
+                .build();
+
+        TestPojo convertedPojo = converter.convert(jsonObject, TestPojo.class);
+        assertThat(convertedPojo.getTestAttribute(), is("testValue"));
+    }
+
+    private class TestPojo {
+
+        private String testAttribute;
+
+        public String getTestAttribute() {
+            return testAttribute;
+        }
+
+        public void setTestAttribute(String testAttribute) {
+            this.testAttribute = testAttribute;
+        }
+
+    }
+}

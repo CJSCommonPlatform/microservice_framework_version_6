6125c90df6f3690eb454a0bcf12407d933f1e7c7
Martin Spasov <martin.y.spasov@gmail.com>
2018-03-29 16:35:57 +0100

    Add a producer for the SubscriptionManager

--- event-sourcing/subscription-manager/src/main/java/uk/gov/justice/services/event/sourcing/subscription/SubscriptionDescriptorRegistry.java
+++ event-sourcing/subscription-manager/src/main/java/uk/gov/justice/services/event/sourcing/subscription/SubscriptionDescriptorRegistry.java
@@ -1,9 +1,12 @@
 package uk.gov.justice.services.event.sourcing.subscription;
 
+import static java.lang.String.format;
 import static java.util.Optional.ofNullable;
 
+import uk.gov.justice.subscription.domain.Subscription;
 import uk.gov.justice.subscription.domain.SubscriptionDescriptor;
 
+import java.util.Collection;
 import java.util.Map;
 import java.util.Optional;
 
@@ -15,7 +18,17 @@ public class SubscriptionDescriptorRegistry {
         this.registry = registry;
     }
 
-    public Optional<SubscriptionDescriptor> getSubscriptionFor(final String serviceName) {
+    public Optional<SubscriptionDescriptor> getSubscriptionDescriptorFor(final String serviceName) {
         return ofNullable(registry.get(serviceName));
+    }
+
+    public Subscription getSubscription(final String subscriptionName) {
+
+        return registry.values().stream()
+                .map(SubscriptionDescriptor::getSubscriptions)
+                .flatMap(Collection::stream)
+                .filter(subscription -> subscription.getName().equals(subscriptionName))
+                .findFirst()
+                .orElseThrow(() -> new SubscriptionManagerProducerException(format("Failed to find subscription '%s' in registry", subscriptionName)));
     }
 }

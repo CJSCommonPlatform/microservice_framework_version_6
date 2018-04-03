a94e98d06a780cf54293465b872766fb0d491f48
amckenzie <allan.mckenzie@agilesphere.co.uk>
2018-04-03 15:42:24 +0100

    Add SubscriptionName CDI Qualifier

---
+++ core/src/main/java/uk/gov/justice/services/core/cdi/SubscriptionName.java
@@ -0,0 +1,18 @@
+package uk.gov.justice.services.core.cdi;
+
+import static java.lang.annotation.ElementType.TYPE;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+@Target(TYPE)
+public @interface SubscriptionName {
+    String value();
+}
+
+

25d5f85e283b22e2ea70e38ceef36e13f0287a36
Justin Cooke <jrrcooke@gmail.com>
2018-04-18 11:11:12 +0100

    Add streamId to enveloper metadata

--- example-context/example-service/example-it/src/test/java/uk/gov/justice/services/example/cakeshop/it/CakeShopIT.java
+++ example-context/example-service/example-it/src/test/java/uk/gov/justice/services/example/cakeshop/it/CakeShopIT.java
@@ -602,6 +602,7 @@ public class CakeShopIT {
                 final TextMessage message = (TextMessage) publicTopicConsumer.receive();
                 with(message.getText())
                         .assertThat("$._metadata.name", equalTo("example.recipe-added"))
+                        .assertThat("$._metadata.stream.id", equalTo(recipeId))
                         .assertThat("$.recipeId", equalTo(recipeId))
                         .assertThat("$.name", equalTo("Apple pie"));
             }

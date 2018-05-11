7ede7d99f715c738c422e3b12507959a5e1db178
Martin Spasov <martin.y.spasov@gmail.com>
2018-05-11 13:45:11 +0100

    Add EventSourceTransformation implementation.

---
+++ event-sourcing/event-source/src/main/java/uk/gov/justice/services/eventsourcing/source/core/EventSourceTransformationProducer.java
@@ -0,0 +1,48 @@
+package uk.gov.justice.services.eventsourcing.source.core;
+
+import uk.gov.justice.services.eventsourcing.repository.jdbc.EventRepository;
+import uk.gov.justice.services.eventsourcing.repository.jdbc.EventRepositoryFactory;
+import uk.gov.justice.services.eventsourcing.repository.jdbc.event.EventJdbcRepository;
+import uk.gov.justice.services.eventsourcing.repository.jdbc.event.EventJdbcRepositoryFactory;
+import uk.gov.justice.services.eventsourcing.repository.jdbc.eventstream.EventStreamJdbcRepository;
+import uk.gov.justice.services.eventsourcing.repository.jdbc.eventstream.EventStreamJdbcRepositoryFactory;
+import uk.gov.justice.services.jdbc.persistence.DataSourceJndiNameProvider;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+
+@ApplicationScoped
+public class EventSourceTransformationProducer {
+
+    @Inject
+    EventStreamManagerFactory eventStreamManagerFactory;
+
+    @Inject
+    EventRepositoryFactory eventRepositoryFactory;
+
+    @Inject
+    EventJdbcRepositoryFactory eventJdbcRepositoryFactory;
+
+    @Inject
+    EventStreamJdbcRepositoryFactory eventStreamJdbcRepositoryFactory;
+
+    @Inject
+    DataSourceJndiNameProvider dataSourceJndiNameProvider;
+
+    @Produces
+    public EventSourceTransformation eventSourceTransformation() {
+
+        final String jndiDatasource = dataSourceJndiNameProvider.jndiName();
+        final EventJdbcRepository eventJdbcRepository = eventJdbcRepositoryFactory.eventJdbcRepository(jndiDatasource);
+        final EventStreamJdbcRepository eventStreamJdbcRepository = eventStreamJdbcRepositoryFactory.eventStreamJdbcRepository(jndiDatasource);
+
+        final EventRepository eventRepository = eventRepositoryFactory.eventRepository(
+                eventJdbcRepository,
+                eventStreamJdbcRepository);
+
+        final EventStreamManager eventStreamManager = eventStreamManagerFactory.eventStreamManager(eventRepository);
+
+        return new DefaultEventSourceTransformation(eventStreamManager);
+    }
+}
---
+++ event-sourcing/event-source/src/test/java/uk/gov/justice/services/eventsourcing/source/core/EventSourceTransformationProducerTest.java
@@ -0,0 +1,66 @@
+package uk.gov.justice.services.eventsourcing.source.core;
+
+import static java.util.Optional.of;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static uk.gov.justice.services.test.utils.core.reflection.ReflectionUtil.fieldValue;
+
+import uk.gov.justice.services.eventsourcing.repository.jdbc.EventRepository;
+import uk.gov.justice.services.eventsourcing.repository.jdbc.EventRepositoryFactory;
+import uk.gov.justice.services.eventsourcing.repository.jdbc.event.EventJdbcRepository;
+import uk.gov.justice.services.eventsourcing.repository.jdbc.event.EventJdbcRepositoryFactory;
+import uk.gov.justice.services.eventsourcing.repository.jdbc.eventstream.EventStreamJdbcRepository;
+import uk.gov.justice.services.eventsourcing.repository.jdbc.eventstream.EventStreamJdbcRepositoryFactory;
+import uk.gov.justice.services.jdbc.persistence.DataSourceJndiNameProvider;
+
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EventSourceTransformationProducerTest {
+
+    @Mock
+    private EventStreamManagerFactory eventStreamManagerFactory;
+
+    @Mock
+    private EventRepositoryFactory eventRepositoryFactory;
+
+    @Mock
+    private EventJdbcRepositoryFactory eventJdbcRepositoryFactory;
+
+    @Mock
+    private EventStreamJdbcRepositoryFactory eventStreamJdbcRepositoryFactory;
+
+    @Mock
+    private DataSourceJndiNameProvider dataSourceJndiNameProvider;
+
+    @InjectMocks
+    private EventSourceTransformationProducer eventSourceTransformationProducer;
+
+    @Test
+    public void shouldCreateEventSourceTransformation() throws Exception {
+        final EventRepository eventRepository = mock(EventRepository.class);
+        final EventStreamManager eventStreamManager = mock(EventStreamManager.class);
+
+        when(eventRepositoryFactory.eventRepository(any(EventJdbcRepository.class), any(EventStreamJdbcRepository.class))).thenReturn(eventRepository);
+        when(eventStreamManagerFactory.eventStreamManager(eventRepository)).thenReturn(eventStreamManager);
+
+        final EventSourceTransformation eventSourceTransformation = eventSourceTransformationProducer.eventSourceTransformation();
+
+        assertThat(eventSourceTransformation, is(instanceOf(DefaultEventSourceTransformation.class)));
+
+        final DefaultEventSourceTransformation defaultEventSourceTransformation = (DefaultEventSourceTransformation) eventSourceTransformation;
+
+        final Optional<Object> eventStreamManagerField = fieldValue(defaultEventSourceTransformation, "eventStreamManager");
+        assertThat(eventStreamManagerField, is(of(eventStreamManager)));
+    }
+}

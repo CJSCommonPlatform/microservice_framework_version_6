9b9049100fef526d339648b75cbabc7d65dbf327
Mohamed Farouk <mohamed.farouk@hmcts.net>
2018-05-21 15:42:20 +0000

    add default event source

--- event-sourcing/event-subscription/src/main/java/uk/gov/justice/subscription/domain/eventsource/EventSourceDefinition.java
+++ event-sourcing/event-subscription/src/main/java/uk/gov/justice/subscription/domain/eventsource/EventSourceDefinition.java
@@ -4,10 +4,14 @@ public class EventSourceDefinition {
 
     private final String name;
     private final Location location;
+    private final boolean defaultEventSource;
 
-    public EventSourceDefinition(final String name, final Location location) {
+    public EventSourceDefinition(final String name,
+                                 final Location location,
+                                 final boolean defaultEventSource) {
         this.name = name;
         this.location = location;
+        this.defaultEventSource = defaultEventSource;
     }
 
     public String getName() {
@@ -17,4 +21,9 @@ public class EventSourceDefinition {
     public Location getLocation() {
         return location;
     }
+
+    public boolean isDefaultEventSource() {
+        return defaultEventSource;
+    }
+
 }

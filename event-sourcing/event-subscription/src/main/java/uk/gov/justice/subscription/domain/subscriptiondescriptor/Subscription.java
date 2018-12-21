4ce31c8b96ea05ab3bbf5d047b8676fd47906a60
amckenzie <allan.mckenzie@agilesphere.co.uk>
2018-12-21 15:16:23 +0000

    Fix CDI scope of catchup to be RequestScoped

--- event-sourcing/event-subscription/src/main/java/uk/gov/justice/subscription/domain/subscriptiondescriptor/Subscription.java
+++ event-sourcing/event-subscription/src/main/java/uk/gov/justice/subscription/domain/subscriptiondescriptor/Subscription.java
@@ -1,6 +1,7 @@
 package uk.gov.justice.subscription.domain.subscriptiondescriptor;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Subscription {
 
@@ -26,5 +27,20 @@ public class Subscription {
 
     public String getEventSourceName() {
         return eventSourceName;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Subscription that = (Subscription) o;
+        return Objects.equals(name, that.name) &&
+                Objects.equals(events, that.events) &&
+                Objects.equals(eventSourceName, that.eventSourceName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, events, eventSourceName);
     }
 }

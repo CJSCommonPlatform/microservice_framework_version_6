ab2fce5182b66d614bab8ad934d4d559682d2f55
Shaun Francis <shaun@mapingo.co.uk>
2019-05-17 12:21:56 +0100

    Add rebuild JMX service bean

--- jmx/src/main/java/uk/gov/justice/services/jmx/MBeanFactory.java
+++ jmx/src/main/java/uk/gov/justice/services/jmx/MBeanFactory.java
@@ -10,9 +10,11 @@ public class MBeanFactory {
 
     private static final String SHUTTERING_DOMAIN_NAME = "shuttering";
     private static final String CATCHUP_DOMAIN_NAME = "catchup";
+    private static final String REBUILD_DOMAIN_NAME = "rebuild";
 
     private static final String SHUTTERING_BEAN = "Shuttering";
     private static final String CATCHUP_BEAN = "Catchup";
+    private static final String REBUILD_BEAN = "Rebuild";
     private static final String OBJECT_NAME_KEY = "type";
 
     @Inject
@@ -20,6 +22,9 @@ public class MBeanFactory {
 
     @Inject
     private Catchup catchup;
+
+    @Inject
+    private Rebuild rebuild;
 
     @Inject
     private ObjectNameFactory objectNameFactory;
@@ -30,6 +35,7 @@ public class MBeanFactory {
 
         mbeanMap.put(shuttering, objectNameFactory.create(SHUTTERING_DOMAIN_NAME, OBJECT_NAME_KEY, SHUTTERING_BEAN));
         mbeanMap.put(catchup, objectNameFactory.create(CATCHUP_DOMAIN_NAME, OBJECT_NAME_KEY, CATCHUP_BEAN));
+        mbeanMap.put(rebuild, objectNameFactory.create(REBUILD_DOMAIN_NAME, OBJECT_NAME_KEY, REBUILD_BEAN));
 
         return mbeanMap;
     }

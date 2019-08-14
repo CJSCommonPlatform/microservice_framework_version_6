a4c742f160fe31bb51a498eaf20934767f25bff1
amckenzie <allan.mckenzie@agilesphere.co.uk>
2019-08-14 11:55:18 +0100

    Make shuttering idempotent

---
+++ framework-system/framework-management/src/main/java/uk/gov/justice/services/management/shuttering/observers/shuttering/ContextShutteredState.java
@@ -0,0 +1,9 @@
+package uk.gov.justice.services.management.shuttering.observers.shuttering;
+
+public enum ContextShutteredState {
+
+    SHUTTERING_STARTED,
+    CONTEXT_SHUTTERED,
+    UNSHUTTERING_STARTED,
+    CONTEXT_UNSHUTTERED
+}

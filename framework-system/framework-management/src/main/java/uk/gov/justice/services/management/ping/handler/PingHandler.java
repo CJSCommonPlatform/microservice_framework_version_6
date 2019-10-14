bec2a336ad897080f9fbecfa3703dd33c9b32178
amckenzie <allan.mckenzie@agilesphere.co.uk>
2019-10-14 11:11:12 +0100

    Add command state to PING command

--- framework-system/framework-management/src/main/java/uk/gov/justice/services/management/ping/handler/PingHandler.java
+++ framework-system/framework-management/src/main/java/uk/gov/justice/services/management/ping/handler/PingHandler.java
@@ -1,13 +1,18 @@
 package uk.gov.justice.services.management.ping.handler;
 
 import static uk.gov.justice.services.jmx.api.command.PingCommand.PING;
+import static uk.gov.justice.services.jmx.api.domain.CommandState.COMMAND_COMPLETE;
+import static uk.gov.justice.services.jmx.api.domain.CommandState.COMMAND_IN_PROGRESS;
 
 import uk.gov.justice.services.common.util.UtcClock;
 import uk.gov.justice.services.jmx.api.command.PingCommand;
 import uk.gov.justice.services.jmx.command.HandlesSystemCommand;
+import uk.gov.justice.services.jmx.state.events.SystemCommandStateChangedEvent;
 
+import java.time.ZonedDateTime;
 import java.util.UUID;
 
+import javax.enterprise.event.Event;
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
@@ -15,13 +20,34 @@ import org.slf4j.Logger;
 public class PingHandler {
 
     @Inject
-    private Logger logger;
+    private Event<SystemCommandStateChangedEvent> systemCommandStateChangedEventFirer;
 
     @Inject
     private UtcClock clock;
 
+    @Inject
+    private Logger logger;
+
     @HandlesSystemCommand(PING)
     public void ping(@SuppressWarnings("unused") final PingCommand pingCommand, @SuppressWarnings("unused") final UUID commandId) {
-        logger.info("********** Received system command '" + PING + "' at " + clock.now() + " **********");
+
+        final ZonedDateTime startedAt = clock.now();
+        systemCommandStateChangedEventFirer.fire(new SystemCommandStateChangedEvent(
+                commandId,
+                pingCommand,
+                COMMAND_IN_PROGRESS,
+                startedAt,
+                "Ping command received"
+        ));
+
+        logger.info("********** Received system command '" + PING + "' at " + startedAt + " **********");
+
+        systemCommandStateChangedEventFirer.fire(new SystemCommandStateChangedEvent(
+                commandId,
+                pingCommand,
+                COMMAND_COMPLETE,
+                clock.now(),
+                "Ping command complete"
+        ));
     }
 }
--- framework-system/framework-management/src/test/java/uk/gov/justice/services/management/ping/handler/PingHandlerTest.java
+++ framework-system/framework-management/src/test/java/uk/gov/justice/services/management/ping/handler/PingHandlerTest.java
@@ -3,16 +3,28 @@ package uk.gov.justice.services.management.ping.handler;
 import static java.time.ZoneOffset.UTC;
 import static java.time.ZonedDateTime.of;
 import static java.util.UUID.randomUUID;
-import static org.mockito.Mockito.verify;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.when;
+import static uk.gov.justice.services.jmx.api.domain.CommandState.COMMAND_COMPLETE;
+import static uk.gov.justice.services.jmx.api.domain.CommandState.COMMAND_IN_PROGRESS;
 
 import uk.gov.justice.services.common.util.UtcClock;
 import uk.gov.justice.services.jmx.api.command.PingCommand;
+import uk.gov.justice.services.jmx.state.events.SystemCommandStateChangedEvent;
 
 import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import javax.enterprise.event.Event;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
@@ -27,19 +39,49 @@ public class PingHandlerTest {
     @Mock
     private Logger logger;
 
+    @Mock
+    private Event<SystemCommandStateChangedEvent> systemCommandStateChangedEventFirer;
+
     @InjectMocks
     private PingHandler pingHandler;
+
+    @Captor
+    private ArgumentCaptor<SystemCommandStateChangedEvent> systemCommandStateChangedEventCaptor;
 
     @Test
     public void shouldLogAPingMessage() throws Exception {
 
+        final UUID commandId = randomUUID();
+        final PingCommand pingCommand = new PingCommand();
 
-        final ZonedDateTime now = of(2019, 11, 7, 15, 22, 25, 0, UTC);
+        final ZonedDateTime startedAt = of(2019, 11, 7, 15, 22, 25, 0, UTC);
+        final ZonedDateTime completeAt = startedAt.plusSeconds(2);
 
-        when(clock.now()).thenReturn(now);
+        when(clock.now()).thenReturn(startedAt, completeAt);
 
-        pingHandler.ping(new PingCommand(), randomUUID());
+        pingHandler.ping(pingCommand, commandId);
 
-        verify(logger).info("********** Received system command 'PING' at 2019-11-07T15:22:25Z **********");
+        final InOrder inOrder = inOrder(systemCommandStateChangedEventFirer, logger);
+
+        inOrder.verify(systemCommandStateChangedEventFirer).fire(systemCommandStateChangedEventCaptor.capture());
+        inOrder.verify(logger).info("********** Received system command 'PING' at 2019-11-07T15:22:25Z **********");
+        inOrder.verify(systemCommandStateChangedEventFirer).fire(systemCommandStateChangedEventCaptor.capture());
+
+        final List<SystemCommandStateChangedEvent> allValues = systemCommandStateChangedEventCaptor.getAllValues();
+
+        final SystemCommandStateChangedEvent startEvent = allValues.get(0);
+        final SystemCommandStateChangedEvent completeEvent = allValues.get(1);
+
+        assertThat(startEvent.getCommandId(), is(commandId));
+        assertThat(startEvent.getCommandState(), is(COMMAND_IN_PROGRESS));
+        assertThat(startEvent.getSystemCommand(), is(pingCommand));
+        assertThat(startEvent.getStatusChangedAt(), is(startedAt));
+        assertThat(startEvent.getMessage(), is("Ping command received"));
+
+        assertThat(completeEvent.getCommandId(), is(commandId));
+        assertThat(completeEvent.getCommandState(), is(COMMAND_COMPLETE));
+        assertThat(completeEvent.getSystemCommand(), is(pingCommand));
+        assertThat(completeEvent.getStatusChangedAt(), is(completeAt));
+        assertThat(completeEvent.getMessage(), is("Ping command complete"));
     }
 }
